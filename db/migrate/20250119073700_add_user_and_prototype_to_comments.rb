b44dcbf2238da4a0f3bc71d1769738da22457009
henataso <pxqth56v@icloud.com>
2025-01-24 01:15:12 +0900

    four commit

--- db/migrate/20250119073700_add_user_and_prototype_to_comments.rb
+++ db/migrate/20250119073700_add_user_and_prototype_to_comments.rb
@@ -1,6 +1,6 @@
 class AddUserAndPrototypeToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :user_id, :integer
-    add_reference :comments, :prototype_id, :integer
+    add_reference :comments, :user, foreign_key: true
+    add_reference :comments, :prototype, foreign_key: true
   end
 end
