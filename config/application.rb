ee4a529584085a2fe62d9b5b927aa79a07fc1a22
henataso <pxqth56v@icloud.com>
2025-01-23 23:54:30 +0900

    second commit

--- config/application.rb
+++ config/application.rb
@@ -1,27 +1,16 @@
-ActiveSupport.on_load(:active_storage_blob) do
-  ActiveStorage::Current.host = 'http://localhost:3000'
-end
-
 require_relative "boot"
 
-require "rails/all"
+require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Protospace41635
+module YourApplicationName
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # その他の設定
+    ActiveSupport.on_load(:active_storage_blob) do
+      ActiveStorage::Current.host = 'http://localhost:3000'
+    end
   end
 end
-
